37ec4a9c2c9dffcfcc6095206061e5af2a8d543f
IndalDA_SCS <148204353+IndalDA@users.noreply.github.com>
2025-09-03 20:11:39 +0530

    Update report.py

--- report.py
+++ report.py
@@ -268,7 +268,7 @@ def process_files(validation_errors, all_locations, start_date, end_date, total_
           else:
             cbo =df.copy()
           cbo_df = Loc_master.merge(cbo,left_on='Code',right_on='Division',how='inner')
-          st.write("Columns in cbo_df before selecting:", cbo_df.columns.tolist())
+          #st.write("Columns in cbo_df before selecting:", cbo_df.columns.tolist())
           cbo_f = cbo_df[['"Brand_x"','Dealer Name','Final Location','Account Name','Account City','Account code',
                           'Order Number','Order Date', 'Part No','Pending Qty']]
           cbo_f.rename(columns={'Brand_x':'Brand','Dealer Name':'Dealer','Final Location':'Location',
@@ -332,6 +332,7 @@ def process_files(validation_errors, all_locations, start_date, end_date, total_
     )
 
 #    st.success("🎉 Reports generated successfully!")
+
 
 
 
